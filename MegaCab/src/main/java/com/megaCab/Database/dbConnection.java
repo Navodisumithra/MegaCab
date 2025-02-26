37e5a4989b24230629719c17554032028640dd8d
Navodi Sumithra <navodipahalawela@gmail.com>
2025-02-26 22:33:16 +0530

    new changes

---
+++ MegaCab/src/main/java/com/megaCab/Database/dbConnection.java
@@ -0,0 +1,26 @@
+package com.megaCab.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {private static final String URL = "jdbc:mysql://127.0.0.1:3306/megaCab";
+    private static final String USER = "root";
+    private static final String PASSWORD = ""; // Use the correct password
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database Connected Successfully!");
+            return conn;
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC Driver Not Found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Database Connection Failed!");
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- MegaCab/src/main/webapp/index.jsp
+++ MegaCab/src/main/webapp/index.jsp
@@ -5,7 +5,7 @@
     <title>Mega Cab </title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <style>
-        /* General Reset */
+
         * {
             margin: 0;
             padding: 0;
