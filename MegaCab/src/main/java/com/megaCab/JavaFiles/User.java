43c0ac6db1391912db9e9fdeb35ee2204928cc9e
Navodi Sumithra <navodipahalawela@gmail.com>
2025-03-07 18:27:35 +0530

    Update the three user types

---
+++ MegaCab/src/main/java/com/megaCab/JavaFiles/User.java
@@ -0,0 +1,83 @@
+package com.megaCab.JavaFiles;
+
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private String name;
+    private String address;
+    private String nic;
+    private String telephone;
+    private String userType;
+
+    public User(String id, String username, String password, String userType) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
